c953c19d0d272d1296f2c7bbbdc01ce4a072f315
Louis Aussedat <aussedat.louis@gmail.com>
2022-11-25 17:40:15 +0100

    src: add cmdline options

--- src/main.py
+++ src/main.py
@@ -1,62 +1,29 @@
 #!/usr/bin/env python3
-import signal
-import sys
-import RPi.GPIO as GPIO
-import alsaaudio as audio
-import wave
+import click
+from phone_recorder import PhoneRecorder as PR
 
 BUTTON_GPIO = 3
 
 JINGLE_FILE = "../audio/messagebip.wav"
 
-AUDIO_FRAMERATE = 44100
 AUDIO_OUTPUT_DEVICE = "hw:0,0"
 AUDIO_INPUT_DEVICE = "hw:1,0"
-AUDIO_FORMAT = audio.PCM_FORMAT_S16_LE
-AUDIO_PERIOD_SIZE = 160
 
 
-def play_jingle():
-    print("play_jingle start")
-    f = wave.open(JINGLE_FILE)
-    message_bip_bytes = f.readframes(f.getnframes())
-
-    out = audio.PCM(audio.PCM_PLAYBACK, device=AUDIO_OUTPUT_DEVICE, channels=2,
-                    rate=AUDIO_FRAMERATE, format=AUDIO_FORMAT, periodsize=AUDIO_PERIOD_SIZE)
-
-    i = 0
-    while (i + AUDIO_PERIOD_SIZE < len(message_bip_bytes)) and (not GPIO.input(BUTTON_GPIO)):
-        out.write(message_bip_bytes[i:i + AUDIO_PERIOD_SIZE])
-        i = i + AUDIO_PERIOD_SIZE
-    print("play_jingle stop")
-
-    return not (i + AUDIO_PERIOD_SIZE < len(message_bip_bytes))
-
-
-def record():
-    print("record start")
-    print("record stop")
-
-
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
-
-
-def gpio_evt_callback(channel):
-    print("gpio_evt_callback!")
-    ret = play_jingle()
-
-    if ret:
-        record()
+@click.command()
+@click.option("--audio-output-device", default=AUDIO_OUTPUT_DEVICE,
+              help="audio output device in alsa format.")
+@click.option("--audio-input-device", default=AUDIO_INPUT_DEVICE,
+              help="audio input device in alsa format.")
+@click.option("--jingle", default=JINGLE_FILE, help="audio period size.")
+@click.option("--gpio", default=BUTTON_GPIO, help="gpio")
+def run(
+        audio_output_device, audio_input_device,
+        jingle, gpio):
+    pr = PR(audio_output_device, audio_input_device,
+            jingle, gpio)
+    pr.run()
 
 
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH,
-                          callback=gpio_evt_callback, bouncetime=100)
-
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
+    run()
---
+++ src/phone_recorder.py
@@ -0,0 +1,68 @@
+import signal
+import sys
+import RPi.GPIO as GPIO
+import alsaaudio as audio
+import wave
+
+AUDIO_FRAMERATE = 44100
+AUDIO_FORMAT = audio.PCM_FORMAT_S16_LE
+AUDIO_PERIOD_SIZE = 160
+
+
+class PhoneRecorder:
+    def __init__(self,
+                 audio_output_device, audio_input_device,
+                 jingle, gpio):
+        """constructor"""
+        self.audio_output_device = audio_output_device
+        self.audio_input_device = audio_input_device
+        self.jingle = jingle
+        self.gpio = gpio
+
+    def run(self):
+        """start the process and listen to event on gpio"""
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        GPIO.add_event_detect(self.gpio, GPIO.BOTH,
+                              callback=self.gpio_evt_callback, bouncetime=100)
+
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.pause()
+
+    def play_jingle(self):
+        """play jingle, stop if gpio status change"""
+        print("play_jingle start")
+        f = wave.open(self.jingle)
+        message_bip_bytes = f.readframes(f.getnframes())
+
+        out = audio.PCM(
+            audio.PCM_PLAYBACK, device=self.audio_output_device, channels=2,
+            rate=AUDIO_FRAMERATE, format=AUDIO_FORMAT,
+            periodsize=AUDIO_PERIOD_SIZE)
+
+        i = 0
+        while (i + AUDIO_PERIOD_SIZE < len(message_bip_bytes)) and (not GPIO.input(self.gpio)):
+            out.write(message_bip_bytes[i:i + AUDIO_PERIOD_SIZE])
+            i = i + AUDIO_PERIOD_SIZE
+        print("play_jingle stop")
+
+        return not (i + AUDIO_PERIOD_SIZE < len(message_bip_bytes))
+
+    def record(self):
+        """record audio, stop if gpio status change"""
+        print("record start")
+        print("record stop")
+
+    def signal_handler(self, sig, frame):
+        """cleanup gpio state on signal"""
+        GPIO.cleanup()
+        sys.exit(0)
+
+    def gpio_evt_callback(self, channel):
+        """callback used when gpio state change"""
+        print("gpio_evt_callback!")
+        ret = self.play_jingle()
+
+        if ret:
+            self.record()
